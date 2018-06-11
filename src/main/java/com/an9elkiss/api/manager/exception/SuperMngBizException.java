b00f473eb5f2c41058c99a0c451443f12cd745a2
an9elkiss <an9elkiss@163.com>
2018-06-11 23:44:45 +0800

    small update

---
+++ src/main/java/com/an9elkiss/api/manager/exception/SuperMngBizException.java
@@ -0,0 +1,32 @@
+package com.an9elkiss.api.manager.exception;
+
+public class SuperMngBizException extends RuntimeException {
+
+	private static final long serialVersionUID = 659402166990562586L;
+
+	public SuperMngBizException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperMngBizException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperMngBizException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperMngBizException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperMngBizException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}

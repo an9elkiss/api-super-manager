b164a0bd1100fb3e7e080e10a99526fec316b6cf
李申申 <shenshen.li@baozun.com>
2018-07-25 12:39:04 +0800

    Optimization get wechart accessToken program

--- src/main/java/com/an9elkiss/api/manager/wechart/service/SendTextMessageServiceImpl.java
+++ src/main/java/com/an9elkiss/api/manager/wechart/service/SendTextMessageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.an9elkiss.api.manager.wechart.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,15 +12,21 @@ import com.an9elkiss.api.manager.wechart.model.WeChartMessage;
 import com.an9elkiss.api.manager.wechart.model.WeChartParames;
 import com.an9elkiss.api.manager.wechart.util.SendMessageUtil;
 import com.an9elkiss.api.manager.wechart.util.WeiXinUtil;
+import com.an9elkiss.commons.constant.RedisKeyPrefix;
+import com.an9elkiss.commons.util.JsonUtils;
+import com.an9elkiss.commons.util.spring.RedisUtils;
 
 import net.sf.json.JSONObject;
 
 @Service
-public class SendTextMessageServiceImpl implements SendTextMessageService{
+public class SendTextMessageServiceImpl implements SendTextMessageService {
 
     // 拿到配置文件中的参数信息
     @Autowired
     private WeChartParames weChartParames;
+
+    @Autowired
+    private RedisUtils redisUtils;
 
     /**
      * 
@@ -27,7 +35,7 @@ public class SendTextMessageServiceImpl implements SendTextMessageService{
      * @return
      */
     @Override
-    public JSONObject sendMessage(WeChartMessage wechartMessage){
+    public JSONObject sendMessage(WeChartMessage wechartMessage) {
 
         // 1.创建文本消息对象
         TextMessage message = new TextMessage();
@@ -53,12 +61,23 @@ public class SendTextMessageServiceImpl implements SendTextMessageService{
         text.setContent(wechartMessage.getContent());
         message.setText(text);
 
-        // 2.获取access_token:根据企业id和通讯录密钥获取access_token,并拼接请求url
-        String accessToken = WeiXinUtil.getAccessToken(weChartParames.getCorpId(), weChartParames.getAgentSecret()).getToken();
+        // 2.获取access_token:
+        // 获取Redis中的AccessToken
+        String accessTokenFromRedis = redisUtils.getString(RedisKeyPrefix.SESSION + weChartParames.getCorpId());
+        String token = null;
+        if (StringUtils.isBlank(accessTokenFromRedis)) {
+            // 根据企业id和通讯录密钥获取access_token,并拼接请求url
+            String accessToken = WeiXinUtil.getAccessToken(weChartParames.getCorpId(), weChartParames.getAgentSecret()).getToken();
+            String json = JsonUtils.toString(accessToken);
+            redisUtils.setString(RedisKeyPrefix.SESSION + weChartParames.getCorpId(), json, 110l, TimeUnit.MINUTES);
+            token = accessToken;
+        } else {
+            token = JsonUtils.parse(accessTokenFromRedis, String.class);
+        }
 
         // 3.发送消息：调用业务类，发送消息
         SendMessageUtil sms = new SendMessageUtil();
-        JSONObject sendMessage = sms.sendMessage(accessToken, message);
+        JSONObject sendMessage = sms.sendMessage(token, message);
         return sendMessage;
     }
 
